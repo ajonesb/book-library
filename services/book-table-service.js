c749d10ea003e64258110ab06701b52f9b2dd24a
Alan Jones <alanjones83@gmail.com>
2017-10-26 16:05:35 -0500

    Update book-table-service.js

--- services/book-table-service.js
+++ services/book-table-service.js
@@ -2,13 +2,23 @@ import {Action} from '../classes/action.js';
 import {Horror} from '../classes/horror.js';
 import {Romance} from '../classes/romance.js';
 
-export class BookTable {
+export class BookTableService {
 
     constructor() {
+        this.book = [];
         this.action = [];
         this.horror = [];
         this.romance = [];
     }
 
-    
+    // get book by author 
+    getBookByAuthor(author) {
+        return this.book.find(function(book){
+            return book.author === author;
+        });
+    }
+
+   
+
+
 }
