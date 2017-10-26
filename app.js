178f7b543a81deade6bd0e3022a6027948b7ed29
Alan Jones <alanjones83@gmail.com>
2017-10-26 16:26:36 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-import {Romance} from './classes/romance.js';
-import {Horror} from './classes/horror.js';
-import {Action} from './classes/action.js';
-import {BookTableService} from './services/book-table-service.js';
-
-export class 
