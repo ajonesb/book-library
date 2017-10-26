57c87ea1630a6f90244f5a0d31ca51523a4baf60
Alan Jones <alanjones83@gmail.com>
2017-10-26 16:57:16 -0500

    Update book-table-service.js

--- src/services/book-table-service.js
+++ src/services/book-table-service.js
@@ -18,8 +18,42 @@ export class BookTableService {
         });
     }
 
-    
-   
+    loadData(book) {
+        for (let data of book){
+           switch(data.type) {
+            case 'book':
+               if (this.validateBookData(data)) {
+                   let book = this.loadBook(data);
+                   if (book)
+                        this.books.push(book);
+               }
+               else     {
+                   let e = new DataError('invalid book data', data);
+                   this.errors.push(e);
+               }
+               break;
+            case 'horror':
+               this.horror.push(data);
+               break;
+            case 'action':
+               this.action.push(data);
+               break;
+            case 'drama':
+               this.drama.push(data);
+               break;
+            default:
+               let e = new DataError('Invalid book', data);
+               this.errors.push(e);
+               break;
+           }
+
+        }
+    }
+
+
+
+
+
 
 
 }
