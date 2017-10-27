ffa3e347afce923cbb8ba43f386fb5d1c943c6bd
Alan Jones <alanjones83@gmail.com>
2017-10-27 16:52:20 -0500

    Update book-table-service.js

--- services/book-table-service.js
+++ services/book-table-service.js
@@ -2,14 +2,32 @@
 
 import {Action} from '../classes/action.js';
 import {Horror} from '../classes/horror.js';
-import {Romance} from '../classes/romance.js';
+import {Drama} from '../classes/drama.js';
 
 export class BookTableService {
 
     constructor() {
         this.action = [];
         this.horror = [];
-        this.romance = [];
+        this.drama = [];
+    }
+
+    loadData(library) {
+        //loop through array with for of statement 
+        for (let data of library) { // set variable called data, don't know what category your getting
+                switch(data.type) {
+                    case 'action':
+                        this.action.push(data);
+                        break;
+                    case 'horror':
+                        this.horror.push(data);
+                        break;
+                    case 'drama':
+                        this.drama.push(data);
+                        break;
+                }
+        }
+
     }
 
 }
