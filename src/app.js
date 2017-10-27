aed30d862df2d2d7e5eff87acbe7ed0471bcce60
Alan Jones <alanjones83@gmail.com>
2017-10-27 16:19:00 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+/* import $ from 'jquery'; */
+import {Book} from './classes/book.js';
 import {Romance} from './classes/romance.js';
 import {Horror} from './classes/horror.js';
 import {Action} from './classes/action.js';
+import {library} from './book-library.js';
 import {BookTableService} from './services/book-table-service.js';
 
+/* let bookService = new BookTableService();
+bookService.loadData(book); */
+
+/* let c = new Book();
+console.log(c instanceof Book); */
+
+/* let c = new Book();
+let d = new Romance();
+
+console.log(c);
+console.log(d); */
+
+console.log(library);
