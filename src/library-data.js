30042095e6a3939feb9b1d22d3df905b0a169b39
Alan Jones <alanjones83@gmail.com>
2017-11-02 12:02:58 -0500

    Update library-data.js

--- src/library-data.js
+++ src/library-data.js
@@ -2,41 +2,61 @@ export let library = [
     {
         title: 'IT',
         category: 'Horror',
-        author: 'Stephen King'        
+        author: 'Stephen King',
+        type: 'book'   
     },
     {
         title: 'The Bourne Identity',
         category: 'Action',
-        author: 'Robert Ludlam'
+        author: 'Robert Ludlam',
+        type: 'book'
+       
+    },
+    {
+        title: 'Horror Weekly',
+        category: 'Horror',
+        author: 'Eli Roth',
+        type: 'magazine'
+        
     },
     {
         title: 'The Hunger Games',
         category: 'Action',
-        author: 'Suzanne Collins'
+        author: 'Suzanne Collins',
+        type: 'book'
+        
     },
     {
         title: 'Dracula',
         category: 'Horror',
-        author: 'Bram Stroker'
-    },
-    {
-        title: 'In the Mouth of Madness',
-        category: 'Horror',
-        author: 'John Carpenter'
+        author: 'Bram Stroker',
+        type: 'book'
+        
     },
     {
         title: 'A Beautiful Mind',
         category: 'Drama',
-        author: 'Sylvia Nasar'
+        author: 'Silvia Nasar',
+        type: 'book'
+        
     },
     {
         title: 'Romeo and Juliet',
         category: 'Drama',
-        author: 'William Shakespeare'
+        author: 'William Shakespeare',
+        type: 'book',
     },
     {
         title: 'Hamlet',
         category: 'Drama',
-        author: 'William Shakespeare'
-    }
+        author: 'William Shakespeare',
+        type: 'book',
+    },
+    {
+        title: 'Guitar World',
+        category: 'Music',
+        author: 'Staff',
+        type: 'Magazine',
+    },
+    
 ];
