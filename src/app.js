52960784856a15c8daade47e773aabea18bf4b6c
Alan Jones <alanjones83@gmail.com>
2017-11-02 12:03:25 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import $ from 'jquery';
-import {Books} from './classes/books.js';
 import {library} from './library-data.js';
 import {BookDataService} from './services/book-data-service.js';
 import {DataTable} from './ui/data-table.js';
 
-let headers = "Title Category Author".split(' ');
+let headers = "Title Category Type Author".split(' ');
 let dataService = new BookDataService();
 dataService.loadData(library);
-let dt = new DataTable(headers, dataService.categories);
+let dt = new DataTable(headers, dataService.books);
 
 dt.appendToElement($('body'));
-
