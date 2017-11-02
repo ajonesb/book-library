6a0827817a9547e6c71c4949c24face9c514adb6
Alan Jones <alanjones83@gmail.com>
2017-11-02 09:02:57 -0500

    Update books.js

--- src/classes/books.js
+++ src/classes/books.js
@@ -1,7 +1,6 @@
 export class Books {
     
     constructor(title, category, author) {
-        super(title, category, author);
         this.title = title;
         this.category = category;
         this.author = author;
