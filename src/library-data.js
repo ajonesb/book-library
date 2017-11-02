11fea21cebdc9360e85327c719bf8c7d70426afa
Alan Jones <alanjones83@gmail.com>
2017-11-02 08:29:21 -0500

    Add files via upload

---
+++ src/library-data.js
@@ -0,0 +1,27 @@
+export let library = [
+    {
+        title: 'IT',
+        category: 'Horror',
+        author: 'Stephen King',        
+    },
+    {
+        title: 'The Bourne Identity',
+        category: 'Action',
+        author: 'Robert Ludlam',
+    },
+    {
+        title: 'The Hunger Games',
+        category: 'Action',
+        author: 'Suzanne Collins',
+    },
+    {
+        title: 'Dracula',
+        category: 'Horror',
+        author: 'Bram Stroker',
+    },
+    {
+        title: 'In the Mouth of Madness',
+        category: 'Horror',
+        author: 'John Carpenter',
+    }
+];
