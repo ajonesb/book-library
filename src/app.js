c3a64b13bd49fa2f2ad452c68ebef85fd72d5c54
Alan Jones <alanjones83@gmail.com>
2017-11-02 10:35:50 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import {DataTable} from './ui/data-table.js';
 let headers = "Title Category Author".split(' ');
 let dataService = new BookDataService();
 dataService.loadData(library);
-let dt = new DataTable(headers, dataService.books);
+let dt = new DataTable(headers, dataService.categories);
 
 dt.appendToElement($('body'));
+
