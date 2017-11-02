3b1594d0a5a4ddb32202728178165cfba855c127
Alan Jones <alanjones83@gmail.com>
2017-11-02 12:31:42 -0500

    Delete books.js

--- src/classes/books.js
+++
@@ -1,9 +0,0 @@
-import {ReadingMaterial} from './reading-material.js';
-
-export class Book extends ReadingMaterial {
-    
-    constructor(title, category, author) {
-        super(title, category, author);
-        this.type = null;
-    }
-}
