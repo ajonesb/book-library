fbbd49f2cb8e0e9820d14ba13f1f4c8ccb30b509
Alan Jones <alanjones83@gmail.com>
2017-11-02 08:30:00 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,12 @@
-/* import $ from 'jquery'; */
-import {Book} from './classes/book.js';
-import {Drama} from './classes/drama.js';
-import {Horror} from './classes/horror.js';
-import {Action} from './classes/action.js';
-import {library} from './book-library.js';
-import {BookTableService} from './services/book-table-service.js';
+import $ from 'jquery';
+import {Books} from './classes/books.js';
+import {library} from './library-data.js';
+import {BookDataService} from './services/book-data-service.js';
+import {DataTable} from './ui/data-table.js';
 
-//Testing
-/* let bookService = new BookTableService();
-bookService.loadData(book); */
+let headers = "Title Category Author".split(' ');
+let dataService = new BookDataService();
+dataService.loadData(library);
+let dt = new DataTable(headers, dataService.books);
 
-/* let c = new Book();
-console.log(c instanceof Book); */
-
-/* let c = new Book();
-let d = new Romance();
-
-console.log(c);
-console.log(d); */
-
-// we are getting our objects on the dom
-// console.log(library);
-
-//instantiate our data service 
-let bookService = new BookTableService();
-//create a new method on bookService called loadData
-bookService.loadData(library); // must go into service and implement loadData
-
-/* console.log(bookService.action); // load all action objects*/
-
-// loop through bookService action 
-for (let library of bookService.library)
-    console.log(bookService.library);
-    
+dt.appendToElement($('body'));
