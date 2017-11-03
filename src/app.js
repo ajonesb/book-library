97c2f4932f5e77064957ae8ab196cfe97ca71efa
Alan Jones <alanjones83@gmail.com>
2017-11-03 16:34:11 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import {Magazine} from './classes/magazine.js';
 import {library} from './library-data.js';
 import {BookDataService} from './services/book-data-service.js';
 import {DataTable} from './ui/data-table.js';
+import {SearchInput} from './ui/search-input.js';
 
+let b = new SearchInput();
+b.appendToElement($('body'));
 let headers = "Title Category Type Author".split(' ');
 let dataService = new BookDataService();
 dataService.loadData(library);
 let dt = new DataTable(headers, dataService.books);
-
 dt.appendToElement($('body'));
