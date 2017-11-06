ba9e032905292ef9e35635bc90116ff5ad5b2454
Alan Jones <alanjones83@gmail.com>
2017-11-06 09:47:31 -0500

    Update book-data-service.js

--- src/services/book-data-service.js
+++ src/services/book-data-service.js
@@ -99,6 +99,24 @@ export class BookDataService {
             input.addEventListener('keyup', filterUsers);
     } */
 
+     searchBooks(book) {
+        let input, filter, table, tr, td, i;
+        input = document.getElementById("searchInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+            }       
+        }
+    }
+
     validateBookData(book) {
         let requiredProps = 'title category author type'.split(' ');
         let hasErrors = false;
