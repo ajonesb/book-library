fa9e58e471f738ebaec49c77ef435c294c1b0abd
Alan Jones <alanjones83@gmail.com>
2017-11-06 09:47:59 -0500

    Update search-input.js

--- src/ui/search-input.js
+++ src/ui/search-input.js
@@ -8,26 +8,7 @@ export class SearchInput extends BaseElement {
         }
 
         getElementString() {
-            return `<input type="text" id="searchInput" onkeyup="searchBooks()" placeholder="Search for names.." title="Type in a name">
+        return `<input type="text" id="searchInput" onkeyup="searchBooks()" placeholder="Search for names.." title="Type in a name">
 `
         }
-       
-        searchBooks(library) {
-        let input, filter, table, tr, td, i;
-        input = document.getElementById("searchInput");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("myTable");
-        tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-            }       
-        }
-    }
-
 }
