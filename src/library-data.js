3477fc660cab0f2b6d8b6bf8403cf9de3b470012
Alan Jones <alanjones83@gmail.com>
2017-11-09 15:56:31 -0500

    Update library-data.js

--- src/library-data.js
+++ src/library-data.js
@@ -71,3 +71,64 @@ export let library = [
         published: '1609'
     }   
 ];
+
+/* let libraryTest = library.filter(function (el) {
+    return (el.category === "Music");
+});
+
+console.log(libraryTest); */
+
+/* Outputs on console
+{title: "Rolling Stone", category: "Music", type: "Magazine", editor: "Jann Wenner", frequency: "Monthly"}length: 1__proto__: Array(0) 
+
+**********************************************************************************/
+
+/* let filteredObjects = library.filter(function (el) {
+    return (el.category === "Drama");
+}).map(function(el) {
+    return el.type;
+}).sort();
+
+console.log(filteredObjects); */
+
+/*Outputs on console 
+(3) ["book", "book", "book"]
+0
+:
+"book"
+1
+:
+"book"
+2
+:
+"book"
+length
+:
+3
+
+**********************************************************************************/
+
+let FilteredResults = library.filter(function (el) {
+    return (el);
+    // onkeyup and loop through arrays with filter to specific onkey typed.
+    // get the dom element and replace its content with the new values of the array
+    this.searchBooks = function(library) {
+            let input, filter, table, tr, td, i;
+                input = document.getElementById("searchInput");
+                filter = input.value.toUpperCase();
+                table = document.getElementById("myTable");
+                tr = table.getElementsByTagName("tr");
+                for (i = 0; i < tr.length; i++) {
+                    td = tr[i].getElementsByTagName("td")[0];
+                    if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                    }       
+                }           
+        }
+});
+
+console.log(FilteredResults);
