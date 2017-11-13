8eb53973647705df7078aaca74c97a0b5b156979
Alan Jones <alanjones83@gmail.com>
2017-11-13 11:04:30 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
+// JQuery 
 import $ from 'jquery';
+
+// Classes
 import {Book} from './classes/book.js';
 import {Magazine} from './classes/magazine.js';
+import {ReadingMaterial} from './classes/reading-material.js';
+
+// Data Objects
 import {library} from './library-data.js';
+
+// Services
 import {BookDataService} from './services/book-data-service.js';
+
+// UI
 import {DataTable} from './ui/data-table.js';
-import {SearchInput} from './ui/search-input.js';
+import {SearchInput} from './ui/search-input.js'; 
+import {BaseElement} from './ui/base-element.js';
 
 
-const b = new SearchInput();
+const b = new SearchInput(); 
 b.appendToElement($('body'));
 const headers = "Title Category Type Author".split(' ');
 let dataService = new BookDataService();
