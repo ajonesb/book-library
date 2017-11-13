9d79256c2d932444034a9008af2e7e259f52d940
Alan Jones <alanjones83@gmail.com>
2017-11-13 16:35:37 -0500

    Update data-table.js

--- src/ui/data-table.js
+++ src/ui/data-table.js
@@ -7,14 +7,16 @@ export class DataTable extends BaseElement {
         this.headers = headers;
         this.data = data;
     }
+
     
     getElementString() {
-        
+     
         let thTags = '';
         for (let h of this.headers) {
             thTags += `<th class="mdl-data-table__cell--non-numeric">${h}</th>\n`;
         }
-        
+
+              
         let trTags = '';
         for (let row of this.data) {
             trTags += `<tr>`;
@@ -28,7 +30,8 @@ export class DataTable extends BaseElement {
             }
             trTags += '</tr>';
         }
-        
+
+       
         return `
         
             <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
@@ -37,7 +40,7 @@ export class DataTable extends BaseElement {
                     ${thTags}
                     </tr>
                 </thead>
-                <tbody id="myTable">
+                <tbody>
                     ${trTags}
                 </tbody>
             </table>
