3a9589a466f0dcd1e0e32eaaad888f9794db7f40
Alan Jones <alanjones83@gmail.com>
2017-11-21 14:08:11 -0500

    Update data-table.js

--- src/ui/data-table.js
+++ src/ui/data-table.js
@@ -19,32 +19,88 @@ export class DataTable extends BaseElement {
               
         let trTags = '';
         for (let row of this.data) {
-            trTags += `<tr>`;
+            trTags += `
+            <tr>
+                <td>
+                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type='button' onclick='bookDisplay(this);' class='btn btn-default'>
+                        Edit Book <span class='glyphicon glyphicon-edit'>
+                        </span>
+                    </button>
+                </td>           
+            `;
             let tdTags = '';
             for (let property of this.headers) {
                 let field = row[property.toLowerCase()];
-                trTags += `<td class="mdl-data-table__cell--non-numeric">
-                             ${field}
-                           </td>
-                          `;
+                trTags += `
+                    <td class="mdl-data-table__cell--non-numeric">
+                                ${field}
+                    </td>
+                    
+                `;
             }
-            trTags += '</tr>';
+                         
+            trTags += ` <td>
+                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type='button' onclick='bookDelete(this);' class='btn btn-default'>
+                    Delete Book<span class='glyphicon glyphicon-remove'>
+                    </span>
+                    </button>
+                </td>
+                </tr>`;
         }
 
        
         return `
-        
-            <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
-                <thead>
-                    <tr>
-                    ${thTags}
-                    </tr>
-                </thead>
-                <tbody>
+        <div id="library-books-container">
+            <h2 style="font-size: 28px;">Book Library</h2>
+            <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">       
+            <div id="box">
+			    <!--Adding new books --> 
+                <div id="new-books-added"></div>
+		    </div>
+                <tbody class="myTable">
                     ${trTags}
                 </tbody>
             </table>
-        `;
+            <div id="add-new-books" class="container">
+                <div class="row">
+                    <!--<div class="col-sm-6">
+                        <h2>ADD NEW BOOK TO LIBRARY</h2>
+                    </div>-->
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h2 style="font-size: 28px;">Book Library Information</h2>
+                            </div>
+                                <div class="panel-body">
+                                <div class="form-group">
+                                    <label for="title">Title</label> <input class="form-control" id="title" type="text" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="category">Category</label> <input class="form-control" id="category" type="text" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="author">Author</label> <input class="form-control" id="author" type="text" value="">
+                                </div>
+                                <div class="form-group">
+                                    <label for="type">Type</label> <input class="form-control" id="type" type="text" value="">
+                                </div>                   
+                            </div>
+                            <div class="panel-footer">
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <button type="button" class="btn btn-primary mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" id="updateButton" onclick="bookUpdate();">+</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            `;
     }
     
 }
