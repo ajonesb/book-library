cc424d7c6508a82c25772000ec490d24c0e2a5ea
Alan Jones <alanjones83@gmail.com>
2017-11-22 18:15:50 -0500

    Update data-table.js

--- src/ui/data-table.js
+++ src/ui/data-table.js
@@ -50,55 +50,67 @@ export class DataTable extends BaseElement {
 
        
         return `
-        <div id="library-books-container">
+        <div class="content-grid mdl-grid" id="library-books-container">
+        <div class="mdl-cell mdl-cell--8-col">
             <h2 style="font-size: 28px;">Book Library</h2>
-            <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">       
-            <div id="box">
-			    <!--Adding new books --> 
-                <div id="new-books-added"></div>
-		    </div>
-                <tbody class="myTable">
-                    ${trTags}
-                </tbody>
-            </table>
-            <div id="add-new-books" class="container">
-                <div class="row">
-                    <!--<div class="col-sm-6">
-                        <h2>ADD NEW BOOK TO LIBRARY</h2>
-                    </div>-->
-                </div>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div class="panel panel-primary">
-                            <div class="panel-heading">
-                                <h2 style="font-size: 28px;">Book Library Information</h2>
-                            </div>
-                                <div class="panel-body">
-                                <div class="form-group">
-                                    <label for="title">Title</label> <input class="form-control" id="title" type="text" value="">
+                    <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">     
+                        <div id="box">
+                            <!--Adding new books --> 
+                            <div id="new-books-added"></div>
+                        </div>
+                            <tbody class="myTable">
+                                ${trTags}
+                            </tbody>
+                    </table>       
+        </div>
+
+        <div class="mdl-cell mdl-cell--4-col">
+                <div id="add-new-books" class="container">
+                    <div class="row">
+                        <!--<div class="col-sm-6">
+                            <h2>ADD NEW BOOK TO LIBRARY</h2>
+                        </div>-->
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <h2 style="font-size: 28px;">Book Library Information</h2>
                                 </div>
-                                <div class="form-group">
-                                    <label for="category">Category</label> <input class="form-control" id="category" type="text" value="">
+                                    <div class="panel-body">
+                                    <div class="form-group">
+                                        <label for="title">Title</label> 
+                                        <input class="form-control mdl-textfield__input" id="title" type="text" value="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="category">Category</label> 
+                                        <input class="form-control mdl-textfield__input" id="category" type="text" value="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="author">Author</label> 
+                                        <input class="form-control mdl-textfield__input" id="author" type="text" value="">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="type">Type</label> 
+                                        <input class="form-control mdl-textfield__input" id="type" type="text" value="">
+                                    </div>                   
                                 </div>
-                                <div class="form-group">
-                                    <label for="author">Author</label> <input class="form-control" id="author" type="text" value="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="type">Type</label> <input class="form-control" id="type" type="text" value="">
-                                </div>                   
-                            </div>
-                            <div class="panel-footer">
-                                <div class="row">
-                                    <div class="col-xs-12">
-                                        <button type="button" class="btn btn-primary mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" id="updateButton" onclick="bookUpdate();">+</button>
+                                <div class="panel-footer">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <button type="button" class="buttonMargin btn btn-primary mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" id="updateButton" onclick="bookUpdate();">+</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
+    </div>
+
+
+
 
             `;
     }
